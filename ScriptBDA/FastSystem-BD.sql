83b0b56cf0484508f21b7dcfcfa6fa94b143786c
Felipe Gomes <felipe.gomes@sptech.school>
2022-09-25 13:41:13 -0300

    Update FastSystem-BD.sql

--- ScriptBDA/FastSystem-BD.sql
+++ ScriptBDA/FastSystem-BD.sql
@@ -2,9 +2,10 @@ CREATE DATABASE FastSystem;
 USE FastSystem;
 
 CREATE TABLE Empresa(
-id_empresa INT PRIMARY KEY NOT NULL,
+id_empresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome_empresa VARCHAR(100),
 cnpj_empresa VARCHAR (14),
+cep_empresa VARCHAR(8),
 numero_empresa INT,
 telefone_empresa VARCHAR(13),
 nome_representante VARCHAR(100),
@@ -13,7 +14,7 @@ senha_empresa VARCHAR(25)
 )AUTO_INCREMENT = 0;
 
 CREATE TABLE Funcionario(
-id_funcionario INT PRIMARY KEY NOT NULL,
+id_funcionario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fk_empresa INT,
 nome_funcionario VARCHAR(100),
 cpf_funcionario VARCHAR(11),
@@ -23,17 +24,16 @@ FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa)
 )AUTO_INCREMENT = 100;
 
 CREATE TABLE Maquina(
-id_maquina INT PRIMARY KEY NOT NULL,
+id_maquina INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fk_empresa INT,
 tipo_maquina VARCHAR(7),
 CHECK (tipo_maquina = 'DESKTOP' or 'TOTEM'),
 nome_identificador VARCHAR(15),
-codigo_serie VARCHAR(20),
 FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa)
 )AUTO_INCREMENT = 0;
 
 CREATE TABLE App(
-id_app INT PRIMARY KEY NOT NULL,
+id_app INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome_app VARCHAR(60),
 funcao VARCHAR(45),
 prioridade INT,
@@ -48,7 +48,7 @@ FOREIGN KEY(fk_app) REFERENCES App(id_app)
 );
 
 CREATE TABLE Componente(
-id_componente INT PRIMARY KEY NOT NULL,
+id_componente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nome_comopoente VARCHAR(45),
 metrica_componente VARCHAR(10),
 alerta_max_componente INT,
@@ -70,3 +70,11 @@ CREATE TABLE Registro(
 data_hora DATETIME,
 medida FLOAT
 );
+
+INSERT INTO Empresa (nome_empresa, cnpj_empresa, cep_empresa, numero_empresa, telefone_empresa, nome_representante, email_empresa, senha_empresa)
+ VALUES ('McDonalds Augusta', '44729194000136', '03273430', 188, '(11)8486-5515', 'Paulo Muzy', 'mcdonalds188@gmail.com', '12345678'),
+		('Popeyes Av.Paulista', '76444561000141', '08474233', 8115, '(11)0568-8515', 'Jorge de Sá', 'popeyes8115@gmail.com', '12345678'),
+        ('McDonalds Av.Paulista', '65708879000176', '04894465', 355, '(11)8941-8115', 'Renato Russo', 'mcdonalds355@gmail.com', '12345678'),
+		('KFC Av.Paulista', '57992929000161', '04913140', 885, '(11)8485-6547', 'Ivete Sangalo', 'kfc885@gmail.com', '12345678');
+
+SELECT * FROM Empresa;
