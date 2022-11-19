2b309592e7a13744fb7a812fac2c5f7c4b8f7c2d
Felipe Gomes <felipe.gomes@sptech.school>
2022-11-19 11:40:38 -0300

    Update-FastSystemBD-v19-11

--- ScriptBDA/FastSystem-BD-v14-11.sql
+++ ScriptBDA/FastSystem-BD-v14-11.sql
@@ -15,7 +15,7 @@ email_empresa VARCHAR(50)
 )AUTO_INCREMENT = 0;
 
 INSERT INTO Empresa VALUES 
-(null, 'FastSystem', '123456789', 02535412, 1522, 11942563656, 'Endryl', "endryl@gmail.com");
+(null, 'Fast Food', '123456789', 02535412, 1522, 11942563656, 'Endryl', "fastfood@gmail.com");
 
 CREATE TABLE Funcionario(
 id_funcionario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -29,14 +29,15 @@ telefone_funcionario VARCHAR(13),
 FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa)
 )AUTO_INCREMENT = 100;
 
-INSERT INTO Funcionario VALUES(null, 1, 'Gerson', 1, '11122233344', 'gerson@gmail.com', '12345678', '(11)912341234');
-
+INSERT INTO Funcionario VALUES(null, 1, 'Gerson', 1, '11122233344', 'gerson@gmail.com', '12345678', '(11)912341234'),
+								(null, 1, 'Gabriel', 0, '11122233344', 'gabriel@gmail.com', '12345678', '(11)912341234');
+                                
 CREATE TABLE Maquina(
 id_maquina INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fk_empresa INT,
+nome_maquina VARCHAR(15),
 tipo_maquina VARCHAR(7),
 CHECK (tipo_maquina = 'DESKTOP' or tipo_maquina = 'TOTEM'),
-nome_maquina VARCHAR(15),
 sistema_operacional_maquina varchar(45),
 tempo_atividade_maquina LONG,
 email_maquina VARCHAR(45),
@@ -45,7 +46,9 @@ FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa)
 )AUTO_INCREMENT = 0;
 
 INSERT INTO Maquina VALUES 
-( null, 1, "DESKTOP", "Desktop 1", '', 0, 'felipe.fastsystem@gmail.com', '1234' );
+( null, 1, "Admin PC", "DESKTOP", 'Windows', 0, 'felipe.fastsystem@gmail.com', '1234' ),
+( null, 1, "Caixa 1", "DESKTOP", 'Windows', 0, 'c1.fastsystem@gmail.com', '12345' ),
+( null, 1, "Totem 2", "TOTEM", 'Windows', 0, 't2.fastsystem@gmail.com', '123456' );
 
 CREATE TABLE Categoria_App(
 id_categoria INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -64,8 +67,6 @@ fk_categoria INT,
 FOREIGN KEY (fk_categoria) REFERENCES Categoria_App(id_categoria)
 )AUTO_INCREMENT = 1000;
 
-insert into App values(null, 'Whatsapp', 'Portal', 3, 8.0, 1);
-
 CREATE TABLE App_Empresa(
 fk_empresa INT,
 fk_app INT,
@@ -73,6 +74,7 @@ FOREIGN KEY(fk_empresa) REFERENCES Empresa(id_empresa),
 FOREIGN KEY(fk_app) REFERENCES App(id_app)
 );
 
+-- Essa tabela que recebe os apps com Looca
 CREATE TABLE Registro_Processo(
 id_registro_processo INT PRIMARY KEY AUTO_INCREMENT,
 nome_processo VARCHAR(45),
@@ -92,7 +94,7 @@ capacidade_componente FLOAT,
 fk_maquina INT,
 FOREIGN KEY (fk_maquina) REFERENCES Maquina(id_maquina)
 );
-    
+
 CREATE TABLE Tipo_Registro(
 id_tipo_registro INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 descricao_tipo VARCHAR(20)
@@ -118,16 +120,16 @@ SELECT id_componente FROM Empresa
 		INNER JOIN Maquina ON Empresa.id_empresa = Maquina.fk_empresa
         INNER JOIN Componente ON Maquina.id_maquina = Componente.fk_maquina
         WHERE id_maquina = 1 and nome_componente LIKE 'Processador%';
-        
+
 SELECT id_componente, nome_componente FROM Empresa
 		INNER JOIN Maquina ON Empresa.id_empresa = Maquina.fk_empresa
         INNER JOIN Componente ON Maquina.id_maquina = Componente.fk_maquina
         WHERE id_maquina = 1;
-        
+
 SELECT * FROM Componente;
-        
+
 UPDATE Componente SET 
-	nome_componente = 'Processadorrrrr', 
+	nome_componente = 'Processadorr', 
 	is_ativo = true, 
     fabricante_componente = 'GenuineIntel', 
     modelo_componente = 'Intel(R) Core(TM) i7-10610U CPU @ 1.80GHz', 
@@ -136,14 +138,14 @@ UPDATE Componente SET
 		WHERE id_componente = 1 AND nome_componente LIKE 'Processador%';
         
 UPDATE Componente SET 
-	nome_componente = 'Memóriaaaa', 
+	nome_componente = 'Memóriaa', 
 	is_ativo = true,
     capacidade_componente = 10,
     fk_maquina = 1
 		WHERE id_componente = 2 AND nome_componente LIKE 'Memória%';
         
 UPDATE Componente SET 
-	nome_componente = 'Disco 1 BEBE', is_ativo = true, modelo_componente = 'SSSTC CA5-8D256-Q79 (Standard disk drives)', capacidade_componente = 256
+	nome_componente = 'Disco 1 B', is_ativo = true, modelo_componente = 'SSSTC CA5-8D256-Q79 (Standard disk drives)', capacidade_componente = 256
     WHERE id_componente = 3 AND nome_componente LIKE 'Disco 1%';
 
 SELECT nome_empresa, nome_maquina, nome_componente FROM Empresa
@@ -177,4 +179,6 @@ SELECT id_maquina, nome_maquina, tipo_maquina, sistema_operacional_maquina, emai
 	INNER JOIN maquina ON Empresa.id_empresa = maquina.fk_empresa
     WHERE fk_empresa = 2;
     
-    SELECT * FROM Registro;
+SELECT * FROM Registro;
+    
+SELECT * FROM Registro_Processo;
